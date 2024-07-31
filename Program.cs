9876699d9846cf648cf76629f2fd2dfb26feb239
Sungmoo Son <coolsw123@gmail.com>
2024-07-31 17:32:27 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Dele_0729
+{
+	internal class Program
+	{
+		public delegate void SendMessage(string message);
+		
+		
+		static void Main(string[] args)
+		{
+
+			SendMessage sayHello, sayGoodbye, multiDelegate;
+
+			sayHello = Hello;
+			sayGoodbye = Goodbye;
+
+			multiDelegate = sayGoodbye + sayHello;
+
+			multiDelegate("황윤모");
+
+			Console.WriteLine();
+
+			multiDelegate -= sayGoodbye;
+
+			multiDelegate("황윤모");
+
+
+
+		}
+
+		public static void Hello(string message)
+		{
+			Console.WriteLine("안녕하세요." + message + "......!");
+		}
+
+		public static void Goodbye(string message)
+		{
+			Console.WriteLine("안녕히가세요." + message + "......!");
+		}
+
+
+		
+	}
+}
